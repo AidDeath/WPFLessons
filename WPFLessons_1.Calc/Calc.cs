3f335713a8ce3831558e6fb3db41ea7e72147a4b
Aid Death <aiddeath@gmail.com>
2021-03-04 11:09:11 +0300

    Add project files.

---
+++ WPFLessons_1.Calc/Calc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFLessons_1.Calc
+{
+    enum Operation
+    {
+        Plus,
+        Minus,
+        Divide,
+        Multiply
+    }
+    class Calc
+    {
+        public double FirstValue { get; set; }
+        public double SecondValue { get; set; }
+        public bool FirstFilled { get; set; }
+        public Operation Operation { get; set; }
+
+        public Calc()
+        {
+            FirstFilled = false;
+        }
+
+        public double DoCalc()
+        {
+            switch (Operation)
+            {
+                case Operation.Plus:
+                    return FirstValue + SecondValue;
+                case Operation.Minus:
+                    return FirstValue - SecondValue;
+                case Operation.Multiply:
+                    return FirstValue * SecondValue;
+                case Operation.Divide:
+                    return FirstValue / SecondValue;
+                default: throw new Exception("Unknown error occured");
+            }
+        }
+    }
+}
---
+++ WPFLessons_1.Calc/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLessons_1.Calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calc _calc = new Calc();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumericButtonPressed(object sender, RoutedEventArgs e)
+        {
+            if (Screen.Text.Contains('=') || Screen.Text.Contains("ERROR")) Screen.Text = "";
+            Screen.Text += ((Button)sender).Content;
+        }
+
+        private void OperationButtonPressed(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Content)
+            {
+                case "+":
+                    _calc.Operation = Operation.Plus;
+                    break;
+                case "-":
+                    _calc.Operation = Operation.Minus;
+                    break;
+                case "*":
+                    _calc.Operation = Operation.Multiply;
+                    break;
+                case "/":
+                    _calc.Operation = Operation.Divide;
+                    break;
+            }
+
+            Screen.Text += ((Button)sender).Content;
+        }
+
+        private void EqualsButtonPressed(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(Screen.Text.Substring(0, Screen.Text.IndexOfAny(new[] { '+', '-', '*', '/' })));
+                double b = double.Parse(Screen.Text.Substring(Screen.Text.IndexOfAny(new[] { '+', '-', '*', '/' }) + 1));
+
+                _calc.FirstValue = a;
+                _calc.SecondValue = b;
+                Screen.Text += $"={_calc.DoCalc()}";
+            }
+            catch
+            {
+                Screen.Text = "ERROR";
+            }
+            finally
+            {
+                _calc = new Calc();
+            }
+        }
+    }
+}
